86db9976a3f815915a4192e68c4e7742e16c681d
Mahdy Mokhtari <mh.mokhtari7@gmail.com>
2025-05-05 13:41:27 +0330

    some bug fixes

--- pipeline.py
+++ pipeline.py
@@ -2,24 +2,21 @@ import subprocess
 import os
 
 # Pre-process
-subprocess.run(["python", r"C:\Users\Mahmodiyan-PC\Desktop\agha alborz\term6\datascience\final project\Speech-Emotion-Recognition-en-\scripts\preprocess.py"])
+subprocess.run(["python", "./scripts/preprocess.py"])
 
 # Feature Extraction
-subprocess.run(["python", r"C:\Users\Mahmodiyan-PC\Desktop\agha alborz\term6\datascience\final project\Speech-Emotion-Recognition-en-\scripts\feature_extraction.py"])
+subprocess.run(["python", "./scripts/feature_extraction.py"])
 
 # Import features to DB
-# arg_file_path = r"C:\Users\NoteBook\Desktop\programing\Data Science\Uni project\final project\Features\final_features_ImportReady.csv"
 arg_file_path = os.path.join(".", "Features", "final_features_ImportReady.csv")
 subprocess.run(["python", "./scripts/import_to_db.py", arg_file_path])
 
 # Load Data to DF
-# subprocess.run(["python", "./scripts/load_data.py"])
+subprocess.run(["python", "./scripts/load_data.py"])
 
-# # Feature Engineering
-# subprocess.run(["python", "./scripts/feature_engineering.py"])
+# Feature Engineering
+subprocess.run(["python", "./scripts/feature_engineering.py"])
 
-# # Import Engineered featuers to DB
-# # arg_eng_file_path = r"C:\Users\NoteBook\Desktop\programing\Data Science\Uni project\final project\Features\final_features_Finalized.csv
-# arg_eng_file_path = os.path.join(".", "Features", "final_features_Finalized.csv")
-# subprocess.run(["python", "./scripts/import_to_db.py", arg_eng_file_path])
-
+# Import Engineered featuers to DB
+arg_eng_file_path = os.path.join(".", "Features", "final_engineered_features.csv")
+subprocess.run(["python", "./scripts/import_to_db.py", arg_eng_file_path])
--- scripts/feature_engineering.py
+++ scripts/feature_engineering.py
@@ -1,14 +1,19 @@
 import pandas as pd
 from database_connection import get_connection
 from sklearn.preprocessing import StandardScaler
+import os
+
+
+### ============= Config
+FEATURE_FOLDER_PATH = os.path.join(".", "Features")    # updated for docker(.. -> .) ##################################
+
+
 
 def load_data_from_db() -> pd.DataFrame:
     engine = get_connection()
     query = "SELECT * FROM features"
     df = pd.read_sql(query, con=engine)
     return df
-
-
 
 df_final_features = load_data_from_db()
 
@@ -19,7 +24,8 @@ feature_cols = df_final_features.columns.difference(meta_cols, sort=False)
 scaler = StandardScaler()
 df_final_features[feature_cols] = scaler.fit_transform(df_final_features[feature_cols])
 
-df_final_features.to_csv(r"F:\csv_features\final_engineered_features.csv")
+csv_file_path = os.path.join(FEATURE_FOLDER_PATH, "final_engineered_features.csv")
+df_final_features.to_csv(csv_file_path)
 
 print("final features have been succesfully engineered")
 
